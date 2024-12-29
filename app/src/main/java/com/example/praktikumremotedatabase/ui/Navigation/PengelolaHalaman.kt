b9c0f96a7a56482bcda0573b98b42651654d755a
Namira Pramadhani <namnam6617@gmail.com>
2024-12-29 19:23:01 +0700

    membuat navigasi yang dimulai dari halaman home dan membuat navigasi di HomeScreen

--- app/src/main/java/com/example/praktikumremotedatabase/ui/Navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/praktikumremotedatabase/ui/Navigation/PengelolaHalaman.kt
@@ -1,11 +1,30 @@
 package com.example.praktikumremotedatabase.ui.Navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.praktikumremotedatabase.ui.View.DestinasiEntry
+import com.example.praktikumremotedatabase.ui.View.DestinasiHome
+import com.example.praktikumremotedatabase.ui.View.HomeScreen
 
 @Composable
 fun PengelolaHalaman(navController: NavHostController = rememberNavController()){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ) {
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToltemEntry = {navController.navigate(DestinasiEntry.route)},
+                onDetailClick = {
 
+                }
+            )
+        }
+    }
 }
