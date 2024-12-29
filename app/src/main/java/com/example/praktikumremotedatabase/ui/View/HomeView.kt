2181d2dd9b9b61bf8d2dde0bff99df803ff77a60
Namira Pramadhani <namnam6617@gmail.com>
2024-12-29 18:40:42 +0700

    menambahkan CustomTopAppBar pada halaman home

--- app/src/main/java/com/example/praktikumremotedatabase/ui/View/HomeView.kt
+++ app/src/main/java/com/example/praktikumremotedatabase/ui/View/HomeView.kt
@@ -1,9 +1,13 @@
 package com.example.praktikumremotedatabase.ui.View
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.praktikumremotedatabase.ui.CustomWidget.CustomTopAppBar
 import com.example.praktikumremotedatabase.ui.Navigation.DestinasiNavigasi
 import com.example.praktikumremotedatabase.ui.viewModel.HomeViewModel
 import com.example.praktikumremotedatabase.ui.viewModel.PenyediaViewModel
@@ -20,4 +24,19 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     onDetailClick: (String) -> Unit = {},
     viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){}
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomTopAppBar(
+                title = DestinasiHome,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getMhs()
+                }
+            )
+        }
+    ) {  }
+}
