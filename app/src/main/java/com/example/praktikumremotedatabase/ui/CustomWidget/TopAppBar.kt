65a9971cf9bea6856d6f3b88ee774b2d47c0d120
Namira Pramadhani <namnam6617@gmail.com>
2024-12-29 15:38:31 +0700

    membuat fungsi CustomTopAppBar dengan parameternya

---
+++ app/src/main/java/com/example/praktikumremotedatabase/ui/CustomWidget/TopAppBar.kt
@@ -0,0 +1,27 @@
+package com.example.praktikumremotedatabase.ui.CustomWidget
+
+import android.icu.text.CaseMap.Title
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {},
+    onRefresh: () -> Unit = {},
+){
+    CenterAlignedTopAppBar(
+        title= { Text(title) },
+        actions = {
+
+        }
+    )
+}
