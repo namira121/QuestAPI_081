72fc9f8087d4bce1291a73d3851f3d0908c1afe7
Namira Pramadhani <namnam6617@gmail.com>
2024-12-29 14:40:27 +0700

    membuat fungsi insertMhs

--- app/src/main/java/com/example/praktikumremotedatabase/ui/viewModel/InsertViewModel.kt
+++ app/src/main/java/com/example/praktikumremotedatabase/ui/viewModel/InsertViewModel.kt
@@ -3,7 +3,9 @@ package com.example.praktikumremotedatabase.ui.viewModel
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.praktikumremotedatabase.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
 
 class InsertViewModel (private val mhs: MahasiswaRepository): ViewModel(){
     var uiState by mutableStateOf(InsertUiState())
@@ -11,5 +13,15 @@ class InsertViewModel (private val mhs: MahasiswaRepository): ViewModel(){
 
     fun updateInsertMhsState(insertUiEvent.InsertUiEvent){
         uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertMhs(){
+        viewModelScope.launch {
+            try{
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhas())
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
     }
 }
