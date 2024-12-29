62fe145f0aeb0af2388835be32036d23eeb3215c
Namira Pramadhani <namnam6617@gmail.com>
2024-12-29 16:00:26 +0700

    menambahkan customwidgetnya dan menambahkan judul, navigateback dan scrollbehavior

--- app/src/main/java/com/example/praktikumremotedatabase/ui/View/InsertView.kt
+++ app/src/main/java/com/example/praktikumremotedatabase/ui/View/InsertView.kt
@@ -1,9 +1,14 @@
 package com.example.praktikumremotedatabase.ui.View
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.praktikumremotedatabase.ui.CustomWidget.CustomTopAppBar
 import com.example.praktikumremotedatabase.ui.Navigation.DestinasiNavigasi
 import com.example.praktikumremotedatabase.ui.viewModel.InsertViewModel
 import com.example.praktikumremotedatabase.ui.viewModel.PenyediaViewModel
@@ -19,4 +24,18 @@ fun EntryMhsScreen(
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){}
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier=modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomTopAppBar(
+                title = DestinasiEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) {  }
+}
