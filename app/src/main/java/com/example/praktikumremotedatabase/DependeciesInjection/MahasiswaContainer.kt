8a043cb6b6245c328c239eda70020fc1986a043d
Namira Pramadhani <namnam6617@gmail.com>
2024-12-29 14:12:48 +0700

    Setelah menambahkan strings, membuat class MahasiswaContainer untuk url, json dan library retrofit

--- app/src/main/java/com/example/praktikumremotedatabase/DependeciesInjection/MahasiswaContainer.kt
+++ app/src/main/java/com/example/praktikumremotedatabase/DependeciesInjection/MahasiswaContainer.kt
@@ -1,6 +1,10 @@
 package com.example.praktikumremotedatabase.DependeciesInjection
 
 import com.example.praktikumremotedatabase.repository.MahasiswaRepository
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
 
 interface AppContainer{
     val mahasiswaRepository: MahasiswaRepository
@@ -8,4 +12,7 @@ interface AppContainer{
 
 class MahasiswaContainer: AppContainer{
     private val baseUrl ="http://10.0.2.2:8000/umyTI/"
+    private val json = Json { ignoreUnknownKeys=true }
+    private val retrofit: Retrofit=Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
 }
