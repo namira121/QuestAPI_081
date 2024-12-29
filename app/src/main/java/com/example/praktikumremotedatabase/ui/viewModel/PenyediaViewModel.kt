8754523da44d76a8aa08bf0d646e2399e5818b2f
Namira Pramadhani <namnam6617@gmail.com>
2024-12-29 15:55:05 +0700

    membuat PenyediaViewModel dan menginisialisasikan home dan insert viewmodel

---
+++ app/src/main/java/com/example/praktikumremotedatabase/ui/viewModel/PenyediaViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.praktikumremotedatabase.ui.viewModel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import coil.transition.Transition
+import com.example.praktikumremotedatabase.MahasiswaApplications
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiMahasiswa().container.mahasiswaRepository)}
+        initializer { InsertViewModel(aplikasiMahasiswa().container.mahasiswaRepository)}
+    }
+    fun CreationExtras.aplikasiMahasiswa():MahasiswaApplications =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
+}
