62f6bd517baceea5ce8d44b7b5105eb85562e362
Namira Pramadhani <namnam6617@gmail.com>
2024-12-29 15:44:00 +0700

    manambahkan scrollbehavior yang didalamnya terdapat arrow untuk menavigasi ke atas

--- app/src/main/java/com/example/praktikumremotedatabase/ui/CustomWidget/TopAppBar.kt
+++ app/src/main/java/com/example/praktikumremotedatabase/ui/CustomWidget/TopAppBar.kt
@@ -3,10 +3,12 @@ package com.example.praktikumremotedatabase.ui.CustomWidget
 import android.icu.text.CaseMap.Title
 import androidx.compose.foundation.clickable
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
@@ -26,8 +28,17 @@ fun CustomTopAppBar(
         title= { Text(title) },
         actions = {
             Icon(imageVector = Icons.Default.Refresh, contentDescription = "", modifier= Modifier.clickable {
-                onRefresh
+                onRefresh()
             })
+        },
+
+        modifier = modifier,
+        scrollBehavior = scrollBehavior, navigationIcon = {
+            if(canNavigateBack){
+                IconButton(onClick = navigateUp) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
+                }
+            }
         }
     )
 }
