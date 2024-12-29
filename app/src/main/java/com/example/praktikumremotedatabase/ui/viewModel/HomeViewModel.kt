df21675863982e51a0795ed48e73ffbfdb968fc8
Namira Pramadhani <namnam6617@gmail.com>
2024-12-29 15:14:09 +0700

    membuat clas HomeViewModel dan variabel mhsUistate

--- app/src/main/java/com/example/praktikumremotedatabase/ui/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/praktikumremotedatabase/ui/viewModel/HomeViewModel.kt
@@ -1,9 +1,21 @@
 package com.example.praktikumremotedatabase.ui.viewModel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.praktikumremotedatabase.model.Mahasiswa
+import com.example.praktikumremotedatabase.repository.MahasiswaRepository
 
 sealed class HomeUiState{
     data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
     object Error : HomeUiState()
     object Loading : HomeUiState()
+}
+
+class HomeViewModel(private val mhs: MahasiswaRepository): ViewModel(){
+    var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+
 }
