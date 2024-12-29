a740c92d1f14caa4f04bf6bddf2aa5ac31e40a58
Namira Pramadhani <namnam6617@gmail.com>
2024-12-29 16:10:04 +0700

    menambahkan entrybody ke dalam innerpadding

--- app/src/main/java/com/example/praktikumremotedatabase/ui/View/InsertView.kt
+++ app/src/main/java/com/example/praktikumremotedatabase/ui/View/InsertView.kt
@@ -1,5 +1,9 @@
 package com.example.praktikumremotedatabase.ui.View
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
@@ -12,6 +16,7 @@ import com.example.praktikumremotedatabase.ui.CustomWidget.CustomTopAppBar
 import com.example.praktikumremotedatabase.ui.Navigation.DestinasiNavigasi
 import com.example.praktikumremotedatabase.ui.viewModel.InsertViewModel
 import com.example.praktikumremotedatabase.ui.viewModel.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 object DestinasiEntry: DestinasiNavigasi{
     override val route= "item_entry"
@@ -37,5 +42,21 @@ fun EntryMhsScreen(
                 navigateUp = navigateBack
             )
         }
-    ) {  }
+    ) {innerPadding ->
+        EntryBody(
+            insertUiState = viewModel.uiState,
+            onSiswaValueChange = viewModel::updateInsertMhsState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertMhs()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
 }
+
