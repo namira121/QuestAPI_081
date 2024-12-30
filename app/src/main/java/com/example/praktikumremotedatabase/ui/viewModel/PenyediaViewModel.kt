61ae141f3fd9adbc58eab1b5b31f9e5c807ffa97
Namira Pramadhani <namnam6617@gmail.com>
2024-12-30 11:09:32 +0700

    menginisialisasikan DetailViewModel

--- app/src/main/java/com/example/praktikumremotedatabase/ui/viewModel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/praktikumremotedatabase/ui/viewModel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.praktikumremotedatabase.ui.viewModel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -11,7 +12,25 @@ object PenyediaViewModel{
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiMahasiswa().container.mahasiswaRepository)}
         initializer { InsertViewModel(aplikasiMahasiswa().container.mahasiswaRepository)}
+        initializer { DetailViewModel(createSavedStateHandle(),MahasiswaApplications().container.mahasiswaRepository)}
     }
     fun CreationExtras.aplikasiMahasiswa():MahasiswaApplications =
         (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
 }
+
+//object PenyediaViewModel {
+//    val Factory = viewModelFactory {
+//        initializer {
+//            val app = this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications
+//            HomeViewModel(app.container.mahasiswaRepository)
+//        }
+//        initializer {
+//            val app = this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications
+//            InsertViewModel(app.container.mahasiswaRepository)
+//        }
+//        initializer {
+//            val app = this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications
+//            DetailViewModel(createSavedStateHandle(), app.container.mahasiswaRepository)
+//        }
+//    }
+//}
